ea495c441873fe11a34b11f160aa1f3f8de148a3
BDawg <tovar.brian@gmail.com>
2013-09-18 21:45:13 -0700

    autoformatting

--- src/com/godelfin/Main.java
+++ src/com/godelfin/Main.java
@@ -2,6 +2,7 @@ package com.godelfin;
 
 import java.util.ArrayList;
 import java.util.logging.*;
+
 import com.godelfin.sieve.Eratosthenes;
 
 public class Main {
@@ -9,9 +10,10 @@ public class Main {
     private static Logger LOGGER = Logger.getLogger("InfoLogging");
 
     public static void main(String[] args) {
-	    Eratosthenes sieve = Eratosthenes.makeSieve();
+        Eratosthenes sieve = Eratosthenes.makeSieve();
         ArrayList<Integer> primes = sieve.primes(100);
 
         LOGGER.info(primes.toString());
     }
 }
+
